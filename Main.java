3d051fb5ac7f693e189824acc22d4d95de27e5bd
Rishav Kumar <lit2021028@iiitl.ac.in>
2022-09-28 13:38:11 +0530

    java program

---
+++ Main.java
@@ -0,0 +1,13 @@
+class Animal{
+    void print(){
+
+    System.out.println("How are you");
+    System.out.println("i ma agood what about you");
+    }
+}
+class Main{
+    public static void main(String args[]){
+        Animal aa= new Animal();
+        aa.print();
+    }
+}
